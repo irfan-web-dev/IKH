91abc56bddd285387b6ffaf4d0a3a3d240035829
irfan-web-dev <irfan.dev03@gmail.com>
2025-05-31 00:48:59 +0500

    funtionality addition

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
- document.addEventListener("DOMContentLoaded", function () {
+      document.addEventListener("DOMContentLoaded", function () {
+        const animateElements = document.querySelectorAll(".animate-on-scroll");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  animateElements.forEach((element) => {
+    observer.observe(element);
+  });
+
         //search button Header
         const searchIcon = document.getElementById("search-icon");
         const searchContainer = document.querySelector(".search-button");
